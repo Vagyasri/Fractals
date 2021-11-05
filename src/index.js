b36b036dee1d3881f73ba1c2798ccfd94e3b0770
Vagyasri <vagyasri2011@gmail.com>
2021-11-05 10:58:58 +0100

    Add function for new perimeter after iteration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import './style.css';
 
-// calculate perimeter of resulting snowflake in each iteration for a equilateral triangle
+// eslint-disable-next-line max-len
+// Question: calculate the perimeter of the resulting snowflake in each iteration for the given length of equilateral triangle.
+
+// tri Length=243 Iterations=3
+// 1728
+
+// tri Length=19683 Iterations=7
+// 442368
+
+// tri Length=531441 Iterations=7
+// 11943936
+
+// tri Length=531441 Iterations=9
+// 21233664
+
 const triPerimeter = (length) => length * 3;
 
 const periAftIterations = (iterations, length) => {
   let perimeter = 0;
-  for (let i = 0; i < iterations; i += 1) {
-    perimeter += (triPerimeter(length) * (4 / 3));
-    length /= 3;
+  for (let i = 1; i <= iterations; i += 1) {
+    perimeter = (triPerimeter(length) * ((4 / 3) ** i));
   }
   return perimeter;
 };
 
-console.log(periAftIterations(1, 3));
+console.log(periAftIterations(9, 531441));
