8921218ecaf480de68d16af016ec62ebd5e07ae0
Vagyasri <vagyasri2011@gmail.com>
2021-11-05 10:29:24 +0100

    Add function for perimeter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+
+// calculate perimeter of resulting snowflake in each iteration for a equilateral triangle
+const triPerimeter = (length) => length * 3;
+
+const periAftIterations = (iterations, length) => {
+  let perimeter = 0;
+  for (let i = 0; i < iterations; i += 1) {
+    perimeter += (triPerimeter(length) * (4 / 3));
+    length /= 3;
+  }
+  return perimeter;
+};
+
+console.log(periAftIterations(1, 3));
