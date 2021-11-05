6e10c0492be8ad801d8f48eb162713f07c5dca16
Vagyasri <vagyasri2011@gmail.com>
2021-11-05 11:37:30 +0100

    Add function for periAftIterations for square

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 
 // eslint-disable-next-line max-len
-// Question: calculate the perimeter of the resulting snowflake in each iteration for the given length of equilateral triangle.
+// Question: calculate the perimeter of the resulting snowflake in each iteration for the given length of square
 
 // Example: triangle Length=9 Iterations=1
 // Output: 36
@@ -14,14 +14,14 @@ import './style.css';
 
 // 4. tri Length=531441 Iterations=9 // 21233664
 
-const triPerimeter = (length) => length * 3;
+const sqPerimeter = (length) => length * 4;
 
 const periAftIterations = (iterations, length) => {
   let perimeter = 0;
   for (let i = 1; i <= iterations; i += 1) {
-    perimeter = Math.round(triPerimeter(length) * ((4 / 3) ** i));
+    perimeter = Math.round(sqPerimeter(length) * ((5 / 3) ** i));
   }
   return perimeter;
 };
 
-console.log(periAftIterations(9, 531441));
+console.log(periAftIterations(1, 9));
